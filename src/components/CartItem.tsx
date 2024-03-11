1b354286ee35cf4506a993339f814a227d736799
tanveer <tanveeratworks@gmail.com>
2024-03-11 12:31:16 +0530

    cartItems styling.

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,7 +1,21 @@
-import {Image, ImageProps, StyleSheet, Text, View} from 'react-native';
+import {
+  Image,
+  ImageProps,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
 import LinearGradient from 'react-native-linear-gradient';
-import {BORDERRADIUS, COLORS, FONTFAMILY, FONTSIZE, SPACING} from '../theme/theme';
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
+import CustomIcon from './CustomIcon';
 
 interface CartItemProps {
   id: string;
@@ -26,7 +40,7 @@ const CartItem: React.FC<CartItemProps> = ({
   incrementCartItemQuantityHandler,
   decrementCartItemQuantityHandler,
 }) => {
-  console.log(name)
+  console.log(name);
   return (
     <View>
       {prices.length != 1 ? (
@@ -39,14 +53,80 @@ const CartItem: React.FC<CartItemProps> = ({
             <Image source={imagelink_square} style={styles.cartIemImage} />
             <View style={styles.cartItemInfo}>
               <View>
-               <Text style={styles.cartItemTitle}>{name}</Text>
-               <Text style={styles.cartItemSubtitle}>{special_ingredient}</Text>
+                <Text style={styles.cartItemTitle}>{name}</Text>
+                <Text style={styles.cartItemSubtitle}>
+                  {special_ingredient}
+                </Text>
+              </View>
+              <View style={styles.cartItemRoastedContainer}>
+                <Text style={styles.cartItemRoastedText}>{roasted}</Text>
               </View>
             </View>
           </View>
+          {prices.map((data: any, index: any) => (
+            <View
+              key={index.toString()}
+              style={styles.cartItemSizeRowContainer}>
+              <View style={styles.cartItemSizeValueContainer}>
+                <View style={styles.sizeBox}>
+                  <Text
+                    style={[
+                      styles.sizeText,
+                      {
+                        fontSize:
+                          type == 'Bean' ? FONTSIZE.size_12 : FONTSIZE.size_16,
+                      },
+                    ]}>
+                    {data.size}
+                  </Text>
+                </View>
+                <Text style={styles.sizeCurrency}>
+                  {data.currency}
+                  <Text style={styles.sizePrice}> {data.price}</Text>
+                </Text>
+              </View>
+              <View style={styles.cartItemSizeValueContainer}>
+                <TouchableOpacity
+                  style={styles.cartItemIcon}
+                  onPress={() => {
+                    decrementCartItemQuantityHandler(id, data.size);
+                  }}>
+                  <CustomIcon
+                    name="minus"
+                    color={COLORS.primaryWhiteHex}
+                    size={FONTSIZE.size_16}
+                  />
+                </TouchableOpacity>
+                <View style={styles.cartItemQuantityContainer}>
+                  <Text style={styles.cartItemQuantityText}>
+                    {data.quantity}
+                  </Text>
+                </View>
+                <TouchableOpacity
+                  style={styles.cartItemIcon}
+                  onPress={() => {
+                    incrementCartItemQuantityHandler(id, data.size);
+                  }}>
+                  <CustomIcon
+                    name="add"
+                    color={COLORS.primaryWhiteHex}
+                    size={FONTSIZE.size_10}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+          ))}
         </LinearGradient>
       ) : (
-        <></>
+        <LinearGradient
+          start={{x: 0, y: 0}}
+          end={{x: 1, y: 1}}
+          colors={[COLORS.primaryGreyHex, COLORS.primaryBlackHex]}
+          style={styles.cartItemSingleLinearGradient}>
+            <View>
+              <Image source={imagelink_square} style={styles.cartItemSingleImage} />
+            </View>
+          </LinearGradient>
       )}
     </View>
   );
@@ -85,5 +165,83 @@ const styles = StyleSheet.create({
     fontFamily: FONTFAMILY.poppins_regular,
     fontSize: FONTSIZE.size_12,
     color: COLORS.secondaryLightGreyHex,
+  },
+  cartItemRoastedContainer: {
+    height: 50,
+    width: 50 * 2 + SPACING.space_20,
+    borderRadius: BORDERRADIUS.radius_15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.primaryDarkGreyHex,
+  },
+  cartItemRoastedText: {
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_10,
+    color: COLORS.primaryWhiteHex,
+  },
+  cartItemSizeRowContainer: {
+    flex: 1,
+    alignItems: 'center',
+    gap: SPACING.space_20,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  cartItemSizeValueContainer: {
+    flex: 1,
+    alignItems: 'center',
+    gap: SPACING.space_20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  sizeBox: {
+    backgroundColor: COLORS.primaryBlackHex,
+    height: 40,
+    width: 100,
+    borderRadius: BORDERRADIUS.radius_10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  sizeText: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    color: COLORS.secondaryLightGreyHex,
+  },
+  sizeCurrency: {
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_18,
+    color: COLORS.primaryOrangeHex,
+  },
+  sizePrice: {
+    color: COLORS.primaryWhiteHex,
+  },
+  cartItemIcon: {
+    backgroundColor: COLORS.primaryOrangeHex,
+    padding: SPACING.space_12,
+    borderRadius: BORDERRADIUS.radius_10,
+  },
+  cartItemQuantityContainer: {
+    backgroundColor: COLORS.primaryBlackHex,
+    width: 80,
+    borderRadius: BORDERRADIUS.radius_10,
+    borderWidth: 2,
+    borderColor: COLORS.primaryOrangeHex,
+    alignItems: 'center',
+    paddingVertical: SPACING.space_4,
+  },
+  cartItemQuantityText: {
+    fontFamily: FONTFAMILY.poppins_semibold,
+    fontSize: FONTSIZE.size_16,
+    color: COLORS.primaryWhiteHex,
+  },
+  cartItemSingleLinearGradient: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: SPACING.space_12,
+    gap: SPACING.space_12,
+    borderRadius: BORDERRADIUS.radius_25,
+  },
+  cartItemSingleImage: {
+    height: 150,
+    width: 150,
+    borderRadius: BORDERRADIUS.radius_20,
   },
 });
